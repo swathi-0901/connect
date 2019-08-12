21a55ee7ab8e0e725c5599bddb1ceca85102005b
swathi-0901 <swathi.kasikala@gmail.com>
2019-08-12 16:18:10 +0530

    change the route for user signup to signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   devise_scope :user do
   get 'login', to: 'devise/sessions#new'
  end
+ devise_scope :user do
+  get 'signup', to: 'devise/registrations#new'
+ end
 end
