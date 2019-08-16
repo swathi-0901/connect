e364c29f5cd40dc983eaa3c0e4c9dc0804f65a44
swathi-0901 <swathi.kasikala@gmail.com>
2019-08-16 20:42:42 +0530

    generated, created posts and category models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190812051653) do
+ActiveRecord::Schema.define(version: 20190816150516) do
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
